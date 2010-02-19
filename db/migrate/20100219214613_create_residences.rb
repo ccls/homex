bd03392491ee04917f09297b309fa48b9f906b0e
Jake <github@jake.otherinbox.com>
2010-02-19 13:53:25 -0800

    Added Residence

---
+++ db/migrate/20100219214613_create_residences.rb
@@ -0,0 +1,17 @@
+class CreateResidences < ActiveRecord::Migration
+	def self.up
+		create_table :residences do |t|
+			t.references :address
+			t.references :subject
+			t.date :moved_in_on
+			t.date :moved_out_on
+			t.boolean :current_residence
+			t.boolean :residence_at_diagnosis
+			t.timestamps
+		end
+	end
+
+	def self.down
+		drop_table :residences
+	end
+end
---
+++ test/unit/residence_test.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ResidenceTest < ActiveSupport::TestCase
+
+	test "should create residence" do
+		assert_difference 'Residence.count' do
+			residence = create_residence
+			assert !residence.new_record?, "#{residence.errors.full_messages.to_sentence}"
+		end
+	end
+
+	test "should belong to an address" do
+		residence = create_residence
+		assert_nil residence.address
+		residence.address = Factory(:address)
+		assert_not_nil residence.address
+	end
+
+	test "should belong to an subject" do
+
+#		flunk
+
+	end
+
+
+protected
+
+	def create_residence(options = {})
+		record = Factory.build(:residence,options)
+		record.save
+		record
+	end
+
+end
