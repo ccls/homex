7a0fa924cd265f6de61ac9358e746bc42345509a
Jake <github@jake.otherinbox.com>
2010-02-25 16:52:56 -0800

    Replaced spaces with tabs

--- lib/workers/shipping_worker.rb
+++ lib/workers/shipping_worker.rb
@@ -3,8 +3,8 @@ class ShippingWorker < BackgrounDRb::MetaWorker
 	def create(args = nil)
 		# this method is called, when worker is loaded for the first time
 
-    # time argument is in seconds
-    add_periodic_timer(3600) { update_shipping_statuses }
+		# time argument is in seconds
+		add_periodic_timer(3600) { update_shipping_statuses }
 	end
 	
 	def update_shipping_statuses
