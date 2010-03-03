b241b04f89eb0ca451943cc58b8cfe35a0c37c7b
Jake <github@jake.otherinbox.com>
2010-03-03 14:48:38 -0800

    Minor mods to Import/Export migrations

--- db/migrate/20100302230357_create_imports.rb
+++ db/migrate/20100302230357_create_imports.rb
@@ -12,14 +12,18 @@ class CreateImports < ActiveRecord::Migration
 			t.string :race
 			t.string :phone_primary
 			t.string :phone_alternate
-			t.date :date_dust_kit_sent
-			t.date :interview_complete_date
-			t.date :date_case_assigned_to_interviewer
+#			t.date :date_dust_kit_sent
+			t.date :dust_kit_sent_on
+#			t.date :interview_complete_date
+			t.date :completed_interview_on
+#			t.date :date_case_assigned_to_interviewer
+			t.date :case_assigned_on
 			t.string :respondent_type
 			t.string :respondent_first_name
 			t.string :respondent_middle_name
 			t.string :respondent_last_name
-			t.date :date_of_last_action
+#			t.date :date_of_last_action
+			t.date :last_action_on
 			t.string :respondent_address_line_1
 			t.string :respondent_address_line_2
 			t.string :respondent_city
--- db/migrate/20100302230616_create_exports.rb
+++ db/migrate/20100302230616_create_exports.rb
@@ -1,14 +1,17 @@
 class CreateExports < ActiveRecord::Migration
 	def self.up
 		create_table :exports do |t|
-			t.integer :child_id
+#			t.integer :child_id
+			t.integer :childid
 			t.string :first_name
 			t.string :middle_name
 			t.string :last_name
 			t.date :diagnosis_date
-			t.integer :pat_id
+#			t.integer :pat_id
+			t.integer :patid
 			t.string :type
-			t.string :order_no
+#			t.string :order_no
+			t.string :orderno
 			t.string :mother_first_name
 			t.string :mother_middle_name
 			t.string :mother_last_name
