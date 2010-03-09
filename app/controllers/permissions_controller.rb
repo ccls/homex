0de20c85088afb57672fabd70818ede4b97c9539
Jake <github@jake.otherinbox.com>
2010-03-08 17:00:24 -0800

    Added caching to permissions

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -2,4 +2,7 @@ class PermissionsController < ApplicationController
 
 	before_filter :may_view_permissions_required
 
+	#	how to expire a non-db related cache???
+	caches_action :index, :layout => false
+
 end
