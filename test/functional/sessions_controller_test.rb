0bec69c415af2959c7b95876d59aedff04fa55c2
Jake <github@jake.otherinbox.com>
2010-03-08 22:30:09 -0800

    Added real login to sessions controller test

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class SessionsControllerTest < ActionController::TestCase
 
 	test "should logout if authenticated" do
-		log_in
+		login_as active_user
 		delete :destroy
 		assert_redirected_to_cas_logout
 	end
