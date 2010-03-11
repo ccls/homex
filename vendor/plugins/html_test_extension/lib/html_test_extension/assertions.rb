6920e50c1956b8b14db6b418832e8793d5df2456
Jake <github@jake.otherinbox.com>
2010-03-11 10:33:21 -0800

    HtmlTestExtension plugin update

--- vendor/plugins/html_test_extension/lib/html_test_extension/assertions.rb
+++ vendor/plugins/html_test_extension/lib/html_test_extension/assertions.rb
@@ -3,10 +3,12 @@ module HtmlTestExtension
 
 		def self.included(base)
 			base.module_eval do
-				def assert_validates_with_verbosity_control(types = nil, 
-					body = nil, url = nil, options = {})
+				def assert_validates_with_verbosity_control(
+					types = nil, body = nil, 
+					url = nil, options = {})
 					options[:verbose] = Html::Test::Validator.verbose
-					assert_validates_without_verbosity_control(types,body,url,options)
+					assert_validates_without_verbosity_control(
+						types, body, url, options )
 				end
 				alias_method_chain :assert_validates, :verbosity_control
 			end
