c3aecafddece069b4889befd1ebbaee623750540
Jake <github@jake.otherinbox.com>
2010-03-15 18:44:46 -0700

    Corrected migration mistakes

--- db/migrate/20100225192635_create_packages.rb
+++ db/migrate/20100225192635_create_packages.rb
@@ -3,11 +3,11 @@ class CreatePackages < ActiveRecord::Migration
 		create_table :packages do |t|
 			t.integer :position
 #			t.string :carrier, :null => false
-			t.string :tracking_number, :null => false
+#			t.string :tracking_number, :null => false
 			t.string :status
 			t.timestamps
 		end
-		add_index :packages, :tracking_number, :unique => true
+#		add_index :packages, :tracking_number, :unique => true
 	end
 
 	def self.down
--- db/migrate/20100315214136_add_tracking_number_to_packages.rb
+++ db/migrate/20100315214136_add_tracking_number_to_packages.rb
@@ -1,6 +1,7 @@
 class AddTrackingNumberToPackages < ActiveRecord::Migration
 	def self.up
 		add_column :packages, :tracking_number, :string
+		add_index :packages, :tracking_number, :unique => true
 	end
 
 	def self.down
