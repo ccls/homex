31372bc67933da5e6738af563b967226405f9601
Jake <github@jake.otherinbox.com>
2010-03-17 12:08:40 -0700

    Moved comments so that rdoc doesn't included them

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,8 +7,8 @@
 #	==	requires
 #	*	name ( unique and > 3 chars )
 class Organization < ActiveRecord::Base
+
 	has_many :aliquots, :foreign_key => 'owner_id'
-#	has_many :samples
 
 	has_many :incoming_transfers, 
 		:foreign_key => 'to_organization_id', 
@@ -16,6 +16,8 @@ class Organization < ActiveRecord::Base
 	has_many :outgoing_transfers, 
 		:foreign_key => 'from_organization_id',
 		:class_name => 'Transfer'
+
+#	has_many :samples
 
 	validates_length_of :name, :minimum => 4
 	validates_uniqueness_of :name
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -16,8 +16,9 @@ class Sample < ActiveRecord::Base
 	belongs_to :sample_subtype
 	belongs_to :subject
 	belongs_to :unit
-#	belongs_to :organization
 	has_many :aliquots
+
+#	belongs_to :organization
 
 	validates_presence_of :subject_id
 	validates_presence_of :unit_id
