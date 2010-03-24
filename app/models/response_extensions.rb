b268a98ba2a1212024a6f0ef6de0cef873468841
Jake <github@jake.otherinbox.com>
2010-03-24 08:53:02 -0700

    Added comment

--- app/models/response_extensions.rb
+++ app/models/response_extensions.rb
@@ -18,6 +18,12 @@ module ResponseExtensions
 			a_code = if self.answer.response_class == "answer"
 				self.answer.data_export_identifier
 			else
+
+#
+#	what if response_class is "" or nil or not
+#	what we need to create *_value???
+#
+
 				self.send("#{self.answer.response_class}_value")
 			end
 			[ q_code, a_code ]
