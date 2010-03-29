24ac7cb95b329dcdd72f1d79ef856acaa9992fbf
Jake <github@jake.otherinbox.com>
2010-03-29 10:49:14 -0700

    Updated ActsAsTrackable plugin

--- db/migrate/20100315214136_add_tracking_info_to_packages.rb
+++ db/migrate/20100315214136_add_tracking_info_to_packages.rb
@@ -1,10 +1,12 @@
-class AddTrackingNumberToPackages < ActiveRecord::Migration
+class AddTrackingInfoToPackages < ActiveRecord::Migration
 	def self.up
+		add_column :packages, :tracks_count, :integer, :default => 0
 		add_column :packages, :tracking_number, :string
 		add_index :packages, :tracking_number, :unique => true
 	end
 
 	def self.down
+		remove_column :packages, :tracks_count
 		remove_column :packages, :tracking_number
 	end
 end
