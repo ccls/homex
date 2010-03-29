97a156807d8e0d5ac40ba52bb025ba095ba96a65
Jake <github@jake.otherinbox.com>
2010-03-29 14:49:32 -0700

    Added indices to survey tables

--- db/migrate/20100322173009_create_validations.rb
+++ db/migrate/20100322173009_create_validations.rb
@@ -1,20 +1,21 @@
 class CreateValidations < ActiveRecord::Migration
-  def self.up
-    create_table :validations do |t|
-      # Context
-      t.integer :answer_id # the answer to validate
-      
-      # Conditional
-      t.string :rule
+	def self.up
+		create_table :validations do |t|
+			# Context
+			t.integer :answer_id # the answer to validate
+			
+			# Conditional
+			t.string :rule
 
-      # Message
-      t.string :message
-      
-      t.timestamps
-    end
-  end
+			# Message
+			t.string :message
+			
+			t.timestamps
+		end
+		add_index :validations, :answer_id
+	end
 
-  def self.down
-    drop_table :validations
-  end
+	def self.down
+		drop_table :validations
+	end
 end
