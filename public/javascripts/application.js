240703b64bc5616eb899796a9eeeced42bd3dd3c
Jake <github@jake.otherinbox.com>
2010-04-07 16:29:14 -0700

    Remove flash when clicked rather than just hide

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,6 @@ jQuery(function(){
 		window.location.href = $(this).find('span.href').text();
 	});
 
-	jQuery('p.flash').click(function(){$(this).hide();});
+	jQuery('p.flash').click(function(){$(this).remove();});
 
 });
