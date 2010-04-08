70da69f20dbc5c1dca9d54417ed3b4943421e930
Jake <github@jake.otherinbox.com>
2010-04-08 11:37:11 -0700

    Deving survey invitations

---
+++ db/migrate/20100408183129_add_unique_subject_id_indexes.rb
@@ -0,0 +1,15 @@
+class AddUniqueSubjectIdIndexes < ActiveRecord::Migration
+	def self.up
+		add_index :piis,               :subject_id, :unique => true
+		add_index :patients,           :subject_id, :unique => true
+		add_index :child_ids,          :subject_id, :unique => true
+		add_index :survey_invitations, :subject_id, :unique => true
+	end
+
+	def self.down
+		remove_index :piis,               :subject_id
+		remove_index :patients,           :subject_id
+		remove_index :child_ids,          :subject_id
+		remove_index :survey_invitations, :subject_id
+	end
+end
