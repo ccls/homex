254608a767723416aa3c5037eab40197da298cdc
Jake <github@jake.otherinbox.com>
2010-04-08 14:18:26 -0700

    Deving survey invitations

--- app/controllers/survey_invitations_controller.rb
+++ app/controllers/survey_invitations_controller.rb
@@ -1,11 +1,12 @@
 class SurveyInvitationsController < ApplicationController
 	before_filter :may_create_invitations_required, :only => :create
 	before_filter :valid_subject_id_required, :only => :create
+	before_filter :valid_survey_required, :only => :create
 
 	before_filter :valid_invitation_token_required, :only => :show
 
 	def create
-		invitation = @subject.recreate_survey_invitation
+		invitation = @subject.recreate_survey_invitation(@survey)
 		if invitation.new_record?
 			flash[:error] = "There was a problem creating the invitation!"
 		else
@@ -31,10 +32,24 @@ protected
 
 	def valid_invitation_token_required
 		if( SurveyInvitation.exists?( :token => params[:id] ) )
-			@invitation = SurveyInvitation.find(:conditions => {
+			@invitation = SurveyInvitation.first(:conditions => {
 				:token => params[:id] })
 		else
-			access_denied("Valid Invitation token required")
+			access_denied("Valid Invitation required")
+		end
+	end
+
+	def valid_survey_required
+		if( Survey.exists?( params[:survey_id] ) )
+			@survey = Survey.find( params[:survey_id] )
+		elsif( Survey.exists?( :access_code => params[:access_code] ) )
+			@survey = Survey.find( :first, :conditions => {
+				:access_code => params[:access_code] })
+		elsif( Survey.exists?( :access_code => params[:survey_code] ) )
+			@survey = Survey.find( :first, :conditions => {
+				:access_code => params[:survey_code] })
+		else
+			access_denied("Valid Survey ID required")
 		end
 	end
 
