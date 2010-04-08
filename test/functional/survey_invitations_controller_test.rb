5f9b81f6defa852849db379cec84db4c246af86c
Jake <github@jake.otherinbox.com>
2010-04-08 14:14:34 -0700

    More tests for creating survey invitations

--- test/functional/survey_invitations_controller_test.rb
+++ test/functional/survey_invitations_controller_test.rb
@@ -2,11 +2,15 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class SurveyInvitationsControllerTest < ActionController::TestCase
 
+	def setup
+		@survey  = Factory(:survey)
+		@subject = Factory(:subject)
+	end
+
 	test "should create invitation for subject with admin login" do
 		login_as admin_user
-		subject = Factory(:subject)
 		assert_difference('SurveyInvitation.count',1) do
-			post :create, :subject_id => subject.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
 		end
 		assert_redirected_to assigns(:subject)
 		assert_not_nil flash[:notice]
@@ -15,9 +19,8 @@ class SurveyInvitationsControllerTest < ActionController::TestCase
 
 	test "should create invitation for subject with employee login" do
 		login_as active_user(:role_name => 'employee')
-		subject = Factory(:subject)
 		assert_difference('SurveyInvitation.count',1) do
-			post :create, :subject_id => subject.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
 		end
 		assert_redirected_to assigns(:subject)
 		assert_not_nil flash[:notice]
@@ -26,9 +29,8 @@ class SurveyInvitationsControllerTest < ActionController::TestCase
 
 	test "should NOT create invitation for subject with just login" do
 		login_as active_user
-		subject = Factory(:subject)
 		assert_difference('SurveyInvitation.count',0) do
-			post :create, :subject_id => subject.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
 		end
 		assert_redirected_to root_path
 		assert_not_nil flash[:error]
@@ -36,9 +38,8 @@ class SurveyInvitationsControllerTest < ActionController::TestCase
 	end
 
 	test "should NOT create invitation for subject without login" do
-		subject = Factory(:subject)
 		assert_difference('SurveyInvitation.count',0) do
-			post :create, :subject_id => subject.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
 		end
 		assert_redirected_to_cas_login
 	end
@@ -47,19 +48,26 @@ class SurveyInvitationsControllerTest < ActionController::TestCase
 		login_as admin_user
 		assert_difference('SurveyInvitation.count',0) {
 		assert_raise(ActionController::RoutingError){
-			post :create
+			post :create, :survey_id => @survey.id
 		} }
+	end
+
+	test "should NOT create invitation without survey" do
+		login_as admin_user
+		assert_difference('SurveyInvitation.count',0) {
+			post :create, :subject_id => @subject.id
+		}
+		assert_not_nil flash[:error]
 	end
 
 	test "should create ONE invitation for subject" do
 		login_as admin_user
-		subject = Factory(:subject)
 		assert_difference('SurveyInvitation.count',1) do
-			post :create, :subject_id => subject.id
-			post :create, :subject_id => subject.id
-			post :create, :subject_id => subject.id
-			post :create, :subject_id => subject.id
-			post :create, :subject_id => subject.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
+			post :create, :subject_id => @subject.id, :survey_id => @survey.id
 		end
 		#	each create above should destroy the previous one
 		#	just creating a new one each time
@@ -68,10 +76,35 @@ class SurveyInvitationsControllerTest < ActionController::TestCase
 		assert_nil flash[:error]
 	end
 
+	test "should create invitation with survey_code" do
+		login_as admin_user
+		assert_difference('SurveyInvitation.count',1) do
+			post :create, :subject_id => @subject.id, 
+				:survey_code => @survey.access_code
+		end
+		assert_redirected_to assigns(:subject)
+		assert_not_nil flash[:notice]
+		assert_nil flash[:error]
+	end
+
+	test "should create invitation with access_code" do
+		login_as admin_user
+		assert_difference('SurveyInvitation.count',1) do
+			post :create, :subject_id => @subject.id, 
+				:access_code => @survey.access_code
+		end
+		assert_redirected_to assigns(:subject)
+		assert_not_nil flash[:notice]
+		assert_nil flash[:error]
+	end
 
 
 
 
+#	show
+
+
+#	TODO
 
 
 end
