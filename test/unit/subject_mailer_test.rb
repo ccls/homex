499717f7ef15ad7b6f85e12f4ded11d961d13e75
Jake <github@jake.otherinbox.com>
2010-04-09 11:46:45 -0700

    Not doing mailer testing yet

--- test/unit/subject_mailer_test.rb
+++ test/unit/subject_mailer_test.rb
@@ -12,20 +12,20 @@ class SubjectMailerTest < ActionMailer::TestCase
 #		assert_equal @expected.encoded, SubjectMailer.create_invitation(@expected.date).encoded
 #	end
 #
-	test "reminder" do
-		@expected.subject = 'SubjectMailer#reminder'
-		@expected.body    = read_fixture('reminder')
-		@expected.date    = Time.now
-
-		assert_equal @expected.encoded, SubjectMailer.create_reminder(@expected.date).encoded
-	end
-
-	test "thank_you" do
-		@expected.subject = 'SubjectMailer#thank_you'
-		@expected.body    = read_fixture('thank_you')
-		@expected.date    = Time.now
-
-		assert_equal @expected.encoded, SubjectMailer.create_thank_you(@expected.date).encoded
-	end
+#	test "reminder" do
+#		@expected.subject = 'SubjectMailer#reminder'
+#		@expected.body    = read_fixture('reminder')
+#		@expected.date    = Time.now
+#
+#		assert_equal @expected.encoded, SubjectMailer.create_reminder(@expected.date).encoded
+#	end
+#
+#	test "thank_you" do
+#		@expected.subject = 'SubjectMailer#thank_you'
+#		@expected.body    = read_fixture('thank_you')
+#		@expected.date    = Time.now
+#
+#		assert_equal @expected.encoded, SubjectMailer.create_thank_you(@expected.date).encoded
+#	end
 
 end
