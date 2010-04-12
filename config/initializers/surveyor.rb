979279e7c6c41b2bfd926cd1f6ce0cfa1870c61b
Jake <github@jake.otherinbox.com>
2010-04-12 10:42:59 -0700

    Commented out unnecessary requires and added layout to surveyor's config file

--- config/initializers/surveyor.rb
+++ config/initializers/surveyor.rb
@@ -1,7 +1,7 @@
 Surveyor::Config.run do |config|
   config['default.relative_url_root'] = nil # "surveys/" # should end with '/'
   config['default.title'] = nil # "You can take these surveys:"
-  config['default.layout'] = 'application' # "surveyor_default"
+  config['default.layout'] = 'survey' # "surveyor_default"
   config['default.index'] =  nil # "/surveys" # or :index_path_method
   config['default.finish'] =  '/survey_finished' # "/surveys" # or :finish_path_method
   config['use_restful_authentication'] = false # set to true to use restful authentication
@@ -11,6 +11,6 @@ end
 # require 'models/survey_extensions' # Extended the survey model
 # require 'helpers/surveyor_helper_extensions' # Extend the surveyor helper
 
-require 'models/survey_extensions'
-require 'models/response_set_extensions'
-require 'models/response_extensions'
+#require 'models/survey_extensions'
+#require 'models/response_set_extensions'
+#require 'models/response_extensions'
