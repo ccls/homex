fe52a985c66ac0bd94d09d4963e6db06c157284b
Jake <github@jake.otherinbox.com>
2010-04-14 17:00:46 -0700

    Disable 'save page order' button if current order is unchanged

--- app/views/javascripts/pages.js
+++ app/views/javascripts/pages.js
@@ -1,18 +1,38 @@
+var initial_page_order;
 jQuery(function(){
 	jQuery('#pages').sortable({
 		axis:'y', 
 		dropOnEmpty:false, 
 		handle:'img.handle', 
+		update:function(event,ui){compare_page_order()},
 		items:'div.page.row'
 	});
-	before = jQuery('#pages').sortable('serialize',{key:'pages[]'})
+
+	jQuery('#save_order').disable();
+
+	initial_page_order = page_order();
 
 	jQuery('form#order_pages').submit(function(){
-		after = jQuery('#pages').sortable('serialize',{key:'pages[]'})
-		if( before == after ) {
-			alert("Page order hasn't changed");
+		if( initial_page_order == page_order() ) {
+			/*
+				Should get here as button should 
+				be disable if not different!
+			*/
+			alert("Page order hasn't changed. Nothing to save.");
 			return false
 		}
 	})
 
 });
+
+function page_order() {
+	return jQuery('#pages').sortable('serialize',{key:'pages[]'});
+}
+
+function compare_page_order(){
+	if( initial_page_order == page_order() ) {
+		jQuery('#save_order').disable();
+	} else {
+		jQuery('#save_order').enable();
+	}
+}
