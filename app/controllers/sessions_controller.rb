7c877f507a435d1cffc45730bde84b86db0789e3
Jake <github@jake.otherinbox.com>
2010-04-19 13:41:27 -0700

    Prep for local login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,18 @@ class SessionsController < ApplicationController	#:nodoc:
 	def destroy
 #		self.current_user.forget_me if logged_in?
 #		cookies.delete :auth_token
+#
+#	for the moment, cal login is required to logout 
+#	so will never do the "else"
+#
+		calnetuid = session[:calnetuid]
 		reset_session
-		CASClient::Frameworks::Rails::Filter.logout(self)
-#		flash[:notice] = "You have been logged out."
-#		redirect_to root_path		
+		if calnetuid
+			CASClient::Frameworks::Rails::Filter.logout(self)
+		else
+			flash[:notice] = "You have been logged out."
+			redirect_to root_path		
+		end
 	end
 
 end
