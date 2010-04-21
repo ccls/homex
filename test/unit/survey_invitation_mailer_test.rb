3d71d2087871d5034a84946c378e7f7f1e4ab813
Jake <github@jake.otherinbox.com>
2010-04-21 11:22:32 -0700

    Added some tests

--- test/unit/survey_invitation_mailer_test.rb
+++ test/unit/survey_invitation_mailer_test.rb
@@ -12,19 +12,70 @@ class SurveyInvitationMailerTest < ActionMailer::TestCase
 		})
 	end
 
-	test "invitation" do
-		mail = SurveyInvitationMailer.create_invitation(@si)
-		assert_match "@example.com", mail.to.first
+	test "should create invitation" do
+		assert_difference('ActionMailer::Base.deliveries.length',0) {
+			mail = SurveyInvitationMailer.create_invitation(@si)
+			assert_match "@example.com", mail.to.first
+		}
 	end
 
-	test "reminder" do
-		mail = SurveyInvitationMailer.create_reminder(@si)
-		assert_match "@example.com", mail.to.first
+	test "should deliver invitation" do
+		assert_difference('ActionMailer::Base.deliveries.length',1) {
+			mail = SurveyInvitationMailer.deliver_invitation(@si)
+			assert_match "@example.com", mail.to.first
+		}
 	end
 
-	test "thank_you" do
-		mail = SurveyInvitationMailer.create_thank_you(@si)
-		assert_match "@example.com", mail.to.first
+	test "should NOT deliver invitation without email" do
+		@si.subject.pii.update_attribute(:email, nil)
+		assert_difference('ActionMailer::Base.deliveries.length',0) {
+		assert_raise(SurveyInvitationMailer::NoEmailAddress){
+			SurveyInvitationMailer.deliver_invitation(@si)
+		} }
+	end
+
+	test "should create reminder" do
+		assert_difference('ActionMailer::Base.deliveries.length',0) {
+			mail = SurveyInvitationMailer.create_reminder(@si)
+			assert_match "@example.com", mail.to.first
+		}
+	end
+
+	test "should deliver reminder" do
+		assert_difference('ActionMailer::Base.deliveries.length',1) {
+			mail = SurveyInvitationMailer.deliver_reminder(@si)
+			assert_match "@example.com", mail.to.first
+		}
+	end
+
+	test "should NOT deliver reminder without email" do
+		@si.subject.pii.update_attribute(:email, nil)
+		assert_difference('ActionMailer::Base.deliveries.length',0) {
+		assert_raise(SurveyInvitationMailer::NoEmailAddress){
+			SurveyInvitationMailer.deliver_reminder(@si)
+		} }
+	end
+
+	test "should create thank_you" do
+		assert_difference('ActionMailer::Base.deliveries.length',0) {
+			mail = SurveyInvitationMailer.create_thank_you(@si)
+			assert_match "@example.com", mail.to.first
+		}
+	end
+
+	test "should deliver thank_you" do
+		assert_difference('ActionMailer::Base.deliveries.length',1) {
+			mail = SurveyInvitationMailer.deliver_thank_you(@si)
+			assert_match "@example.com", mail.to.first
+		}
+	end
+
+	test "should NOT deliver thank_you without email" do
+		@si.subject.pii.update_attribute(:email, nil)
+		assert_difference('ActionMailer::Base.deliveries.length',0) {
+		assert_raise(SurveyInvitationMailer::NoEmailAddress){
+			SurveyInvitationMailer.deliver_thank_you(@si)
+		} }
 	end
 
 end
