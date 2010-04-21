34582400cd85790f56238fcd50937b5b692421d6
Jake <github@jake.otherinbox.com>
2010-04-21 11:22:16 -0700

    Removed unused commented code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,14 +18,6 @@ class ActiveSupport::TestCase
 
 	include FactoryTestHelper
 
-#	def active_user(options={})
-#		u = Factory(:user, options)
-#	end
-#
-#	def admin_user(options={})
-#		u = active_user(options.merge(:role_name => "administrator"))
-#	end
-
 	def login_as( user=nil )
 		uid = ( user.is_a?(User) ) ? user.uid : user
 		if !uid.blank?
@@ -118,44 +110,5 @@ class ActiveSupport::TestCase
 #		CASClient::Frameworks::Rails::Filter.stubs(:login_url).returns(
 #			"https://auth-test.berkeley.edu/cas/login")
 	end
-
-
-#	def full_response(options={})
-#		survey = Factory(:survey)
-#		survey_section = Factory(:survey_section, :survey => survey)
-#		question = Factory(:question, { 
-#			:survey_section => survey_section }.merge(options[:question]||{}))
-#		answer = Factory(:answer, { :question => question,
-#			:response_class => "answer" }.merge(options[:answer]||{}))
-#		response_set = Factory(:response_set, {
-#			:survey => survey}.merge(options[:response_set]||{}))
-#		response = Factory(:response, {
-#			:response_set => response_set,
-#			:question => question, 
-#			:answer => answer}.merge(options[:response]||{})
-#		)
-#	end
-#
-#	def full_response_set(options={})
-#		survey = if options[:survey].is_a?(Survey)
-#			options[:survey]
-#		else
-#			#	assuming that its a hash
-#			Factory(:survey, options[:survey] )
-#		end
-#		survey_section = Factory(:survey_section, :survey => survey)
-#
-#
-#
-#
-#
-#		response_set = Factory(:response_set, {
-#			:survey => survey}.merge(options[:response_set]||{}))
-#
-#
-#
-#
-#		response_set
-#	end
 
 end
