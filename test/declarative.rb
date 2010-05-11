9a221ef75aeeb5d0eafb6fff392650075fca039f
Jake <github@jake.otherinbox.com>
2010-05-11 14:17:12 -0700

    Refactored test

--- test/declarative.rb
+++ test/declarative.rb
@@ -2,16 +2,23 @@ module My
 	module Declarative
 
 		def test(name, &block)
-       test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
-       defined = instance_method(test_name) rescue false
-       raise "#{test_name} is already defined in #{self}" if defined
-       if block_given?
-         define_method("_#{test_name}", &block)
-       else
-         define_method("_#{test_name}") do
-           flunk "No implementation provided for #{name}"
-         end
-       end
+			test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+			defined = instance_method(test_name) rescue false
+			raise "#{test_name} is already defined in #{self}" if defined
+			if block_given?
+				define_method("_#{test_name}", &block)
+#				define_method(test_name) do
+#					print "\n#{self.class.name.gsub(/Test$/,'')} #{name}: "
+#				doing it this way NEVER seems to fail??
+#				tried block.call too
+#					block
+#				end
+			else
+				define_method("_#{test_name}") do
+#				define_method(test_name) do
+					flunk "No implementation provided for #{name}"
+				end
+			end
 
 
 			define_method(test_name) do
@@ -39,11 +46,13 @@ ActiveSupport::TestCase.send(:extend, My::Declarative)
 #
 #	This never seemed to work and at best I would get
 #
-#		NameError: undefined method `test_with_verbosity' for class `ActiveSupport::TestCase'
+#		NameError: undefined method `test_with_verbosity' for 
+#			class `ActiveSupport::TestCase'
 #
-#	or
+#						or
 #
-#	./test/unit/../declarative.rb:26:in `test_without_verbosity': wrong number of arguments (1 for 2) (ArgumentError)
+#		./test/unit/../declarative.rb:26:in `test_without_verbosity': 
+#			wrong number of arguments (1 for 2) (ArgumentError)
 #
 #	In the end, simply overwriting the existing 'test' 
 #	method seemed to be the only way.
