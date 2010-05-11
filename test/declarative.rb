da6074d7409f7cd3d9ef8daef2658e11ce09f3dd
Jake <github@jake.otherinbox.com>
2010-05-11 15:26:14 -0700

    Better implementation of test_with_verbosity

--- test/declarative.rb
+++ test/declarative.rb
@@ -1,29 +1,20 @@
 module My
 	module Declarative
 
-		def test(name, &block)
+#		def self.included(base)
+#			base.extend Methods
+#			base.alias_method_chain :test, :verbosity
+#		end
+#
+#		module Methods
+
+		def test_with_verbosity(name,&block)
+			test_without_verbosity(name,&block)
+
 			test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
-			defined = instance_method(test_name) rescue false
-			raise "#{test_name} is already defined in #{self}" if defined
-			if block_given?
-				define_method("_#{test_name}", &block)
-#				define_method(test_name) do
-#					print "\n#{self.class.name.gsub(/Test$/,'')} #{name}: "
-#				doing it this way NEVER seems to fail??
-#				tried block.call too
-#					block
-#				end
-			else
-				define_method("_#{test_name}") do
-#				define_method(test_name) do
-					flunk "No implementation provided for #{name}"
-				end
-			end
-
-
-			define_method(test_name) do
+			define_method("_#{test_name}_with_verbosity") do
 				print "\n#{self.class.name.gsub(/Test$/,'')} #{name}: "
-				send("_#{test_name}")
+				send("_#{test_name}_without_verbosity")
 			end
 			#
 			#	can't do this.  
@@ -31,14 +22,59 @@ module My
 			#	end up with 2 methods that begin
 			#	with 'test_' so they both get run
 			#
-#			alias_method "do_not_#{test_name}_without_verbosity".to_sym,
-#				test_name
-#			alias_method test_name,
-#				"#{test_name}_with_verbosity".to_sym
+			alias_method "_#{test_name}_without_verbosity".to_sym,
+				test_name
+			alias_method test_name,
+				"_#{test_name}_with_verbosity".to_sym
 		end
+
+#		def test(name, &block)
+#			test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+#			defined = instance_method(test_name) rescue false
+#			raise "#{test_name} is already defined in #{self}" if defined
+#			if block_given?
+#				define_method("_#{test_name}", &block)
+##				define_method(test_name) do
+##					print "\n#{self.class.name.gsub(/Test$/,'')} #{name}: "
+##				doing it this way NEVER seems to fail??
+##				tried block.call too
+##					block
+##				end
+#			else
+#				define_method("_#{test_name}") do
+##				define_method(test_name) do
+#					flunk "No implementation provided for #{name}"
+#				end
+#			end
+#
+#
+#			define_method(test_name) do
+#				print "\n#{self.class.name.gsub(/Test$/,'')} #{name}: "
+#				send("_#{test_name}")
+#			end
+#			#
+#			#	can't do this.  
+#			#		alias_method_chain test_name, :verbosity
+#			#	end up with 2 methods that begin
+#			#	with 'test_' so they both get run
+#			#
+##			alias_method "do_not_#{test_name}_without_verbosity".to_sym,
+##				test_name
+##			alias_method test_name,
+##				"#{test_name}_with_verbosity".to_sym
+#		end
+
+#		end	#	Methods
+
 	end
 end
-ActiveSupport::TestCase.send(:extend, My::Declarative)
+#ActiveSupport::TestCase.send(:include,My::Declarative)
+ActiveSupport::TestCase.extend My::Declarative
+class ActiveSupport::TestCase
+	class<<self
+		alias_method_chain :test, :verbosity
+	end
+end
 #
 #	I really was hoping to use alias_method_chain on test
 #	creating a test_with_verbosity method and a
