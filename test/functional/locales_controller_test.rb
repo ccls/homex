bfecbec01679d3f84d7fb3c24406851a68f2e387
Jake <github@jake.otherinbox.com>
2010-05-12 11:31:54 -0700

    Added another locale test

--- test/functional/locales_controller_test.rb
+++ test/functional/locales_controller_test.rb
@@ -2,10 +2,18 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class LocalesControllerTest < ActionController::TestCase
 
-	test "should set locale" do
+	test "should set locale to en" do
 		assert_nil session[:locale]
 		get :show, :id => 'en'
 		assert_equal 'en', session[:locale]
+	end
+
+	test "should change locale to es" do
+		assert_nil session[:locale]
+		session[:locale] = 'en'
+		assert_equal 'en', session[:locale]
+		get :show, :id => 'es'
+		assert_equal 'es', session[:locale]
 	end
 
 end
