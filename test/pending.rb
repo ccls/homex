ea3e3993360e6946b7ced6b7ac3e9a9356ce19dd
Jake <github@jake.otherinbox.com>
2010-05-12 15:43:45 -0700

    Pretty up the pending output.

--- test/pending.rb
+++ test/pending.rb
@@ -29,11 +29,22 @@ module ActiveSupport
 					end
 
 					caller[0] =~ (/(.*):(.*):in `(.*)'/)
-					@@pending_cases << "#{$3} at #{$1}, line #{$2}"
+#					@@pending_cases << "#{$3} at #{$1}, line #{$2}"
+					#	Gotta remember these as the next Regex will overwrite them.
+					filename   = $1
+					linenumber = $2
+					testmethod = $3
+					model  = self.class.to_s.gsub(/Test$/,'').titleize
+					method = testmethod.gsub(/_/,' ').gsub(/^test /,'')
+					@@pending_cases << "#{model} #{method}:\n\t#{filename} line #{linenumber}"
+#					@@pending_cases << "#{testmethod} at #{filename}, line #{linenumber}"
 					print "P"
 			
 					@@at_exit ||= begin
 						at_exit do
+							#	For some reason, special characters don't always
+							#	print the way you would expect.  White space (tabs)
+							#	and some carriage returns just weren't happening?
 							puts "\nPending Cases:"
 							@@pending_cases.each do |test_case|
 								puts test_case
