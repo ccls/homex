b89becfc9f4b7ffa1b306c3a689d244a151f8a70
Jake <github@jake.otherinbox.com>
2010-05-17 11:54:09 -0700

    Removed references to UCB's CAS and LDAP

--- config/environment.rb
+++ config/environment.rb
@@ -60,7 +60,7 @@ Rails::Initializer.run do |config|
 
 	#	probably will come from http://gemcutter.org/gems/ucb_ldap
 	#	version 1.3.2 as of Jan 25, 2010
-	config.gem "ucb_ldap", :source => "http://gemcutter.org"
+#	config.gem "ucb_ldap", :source => "http://gemcutter.org"
 
 	config.gem 'active_shipping'
 	config.gem 'gravatar'
