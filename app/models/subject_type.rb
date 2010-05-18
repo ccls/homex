33a6f531b75493450e954a2189e6a525bdf19dc6
Jake <github@jake.otherinbox.com>
2010-05-18 12:59:02 -0700

    Removed length validation for subject type as it appears that 1 character is enough

--- app/models/subject_type.rb
+++ app/models/subject_type.rb
@@ -6,6 +6,7 @@ class SubjectType < ActiveRecord::Base
 
 	has_many :subjects
 
-	validates_length_of :description, :minimum => 4
+	validates_presence_of :description
+#	validates_length_of :description, :minimum => 4
 	validates_uniqueness_of :description
 end
