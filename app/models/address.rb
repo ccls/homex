7b9f22e35f0dac618a8bd8e3dbe2f22829866e8b
Jake <github@jake.otherinbox.com>
2010-05-24 13:15:23 -0700

    Added comment

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,10 @@
 class Address < ActiveRecord::Base
+
 	has_many :interview_events
+
+	#	Residences is a rich join.  
+	#	Destroy to not destroy?
 	has_many :residences, :dependent => :destroy
+
 #	has_many :subjects, :through => :residences
 end
