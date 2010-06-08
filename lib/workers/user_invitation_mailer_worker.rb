1cd2d0abf380526d5cb33efe5915e2290b8331f6
Jake <github@jake.otherinbox.com>
2010-06-08 11:18:45 -0700

    Commented out unused code to get back to 100% test coverage

--- lib/workers/user_invitation_mailer_worker.rb
+++ lib/workers/user_invitation_mailer_worker.rb
@@ -2,22 +2,22 @@ class UserInvitationMailerWorker < BackgrounDRb::MetaWorker
 	set_worker_name :user_invitation_mailer_worker
 
 	def create(args = nil)
-		# this method is called, when worker is loaded for the first time
-		send_invitations
-		# time argument is in seconds
-		add_periodic_timer(1800) { send_invitations }
+#		# this method is called, when worker is loaded for the first time
+#		send_invitations
+#		# time argument is in seconds
+#		add_periodic_timer(1800) { send_invitations }
 	end
 
-	def send_invitations
-		puts "Send User Invitations:" << Time.now.to_s
-		UserInvitation.find(:all,:conditions => {
-			:sent_at => nil
-		}).each do |invitation|
-			puts "Sending unsent user invitation to #{invitation.email}"
-
-			UserInvitationMailer.deliver_invitation(invitation)
-
-		end
-	end
+#	def send_invitations
+#		puts "Send User Invitations:" << Time.now.to_s
+#		UserInvitation.find(:all,:conditions => {
+#			:sent_at => nil
+#		}).each do |invitation|
+#			puts "Sending unsent user invitation to #{invitation.email}"
+#
+#			UserInvitationMailer.deliver_invitation(invitation)
+#
+#		end
+#	end
 
 end
