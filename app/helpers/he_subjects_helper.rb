1fa42517c9345ce470cf0bf0b7ddac5003bf7519
Jake <github@jake.otherinbox.com>
2010-06-08 16:46:29 -0700

    Added text option to sort_link helper

--- app/helpers/he_subjects_helper.rb
+++ app/helpers/he_subjects_helper.rb
@@ -1,10 +1,10 @@
 module HeSubjectsHelper
 
-	def sort_link(column)
+	def sort_link(column,text=nil)
 		klass = column.downcase.gsub(/\s+/,'_')
 		dir = ( params[:dir] && params[:dir] == 'asc' ) ? 'desc' : 'asc'
 		s = "<div class='#{klass}'>"
-		s << link_to(column,params.merge(:order => klass,:dir => dir))
+		s << link_to(text||column,params.merge(:order => klass,:dir => dir))
 		s << "</div>"
 		s
 	end
