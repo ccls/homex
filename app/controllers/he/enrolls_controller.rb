984f919937ed60b63d6daa7af59993a519a658bd
Jake <github@jake.otherinbox.com>
2010-06-14 10:01:14 -0700

    Making enrolls' submenu functional

--- app/controllers/he/enrolls_controller.rb
+++ app/controllers/he/enrolls_controller.rb
@@ -5,19 +5,16 @@ class He::EnrollsController < ApplicationController
 	layout 'home_exposure'
 
 	def index
-		he = StudyEvent.find_by_description('Home Exposure')
-		if params[:commit] && params[:commit] == 'download'
-			params[:paginate] = false
-		end
-#		params[:study_events] ||= {}
-#		params[:study_events][he.id] ||= {}
-#		@subjects = Subject.search(params)
-		@subjects = he.subjects.search(params)
+		get_subjects
 		if params[:commit] && params[:commit] == 'download'
 			params[:format] = 'csv'
 			headers["Content-disposition"] = "attachment; " <<
 				"filename=subjects_#{Time.now.to_s(:filename)}.csv" 
 		end
+	end
+
+	def send_letters
+		get_subjects
 	end
 
 	def update_select
@@ -31,6 +28,19 @@ class He::EnrollsController < ApplicationController
 		redirect_to he_enrolls_path(params.delete_keys!(
 			:_method,:authenticity_token,:action,:subjects,:date
 		))
+	end
+
+protected
+
+	def get_subjects
+		he = StudyEvent.find_by_description('Home Exposure')
+		if params[:commit] && params[:commit] == 'download'
+			params[:paginate] = false
+		end
+#		params[:study_events] ||= {}
+#		params[:study_events][he.id] ||= {}
+#		@subjects = Subject.search(params)
+		@subjects = he.subjects.search(params)
 	end
 
 end
