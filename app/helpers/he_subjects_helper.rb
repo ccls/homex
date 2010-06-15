9fc54e9752728fa0d5fdda6c6677bd31219ba5e2
Jake <github@jake.otherinbox.com>
2010-06-15 10:18:28 -0700

    Added arrow to subject sort column

--- app/helpers/he_subjects_helper.rb
+++ app/helpers/he_subjects_helper.rb
@@ -1,10 +1,21 @@
 module HeSubjectsHelper
 
+	#	&uarr; and &darr;
 	def sort_link(column,text=nil)
-		klass = column.downcase.gsub(/\s+/,'_')
+		order = column.downcase.gsub(/\s+/,'_')
 		dir = ( params[:dir] && params[:dir] == 'asc' ) ? 'desc' : 'asc'
-		s = "<div class='#{klass}'>"
-		s << link_to(text||column,params.merge(:order => klass,:dir => dir))
+		link_text = text||column
+		classes = [order]
+		if params[:order] && params[:order] == order
+			classes.push('sorted')
+			link_text << if dir == 'desc'
+				"&nbsp;&darr;"
+			else
+				"&nbsp;&uarr;"
+			end
+		end
+		s = "<div class='#{classes.join(' ')}'>"
+		s << link_to(link_text,params.merge(:order => order,:dir => dir))
 		s << "</div>"
 		s
 	end
