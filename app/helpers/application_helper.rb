95319aacf02cc2602c5a100675e400638484bbc4
Jake <github@jake.otherinbox.com>
2010-06-22 10:44:04 -0700

    Moved some view logic into helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,66 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+	def home_exposure_main_menu
+		s = "<div id='mainmenu'>\n"
+		controller_name = controller.controller_name
+		l = [link_to( "Subjects", he_subjects_path,
+			:class => (controller_name == 'subjects')?'current':nil)]
+		l.push(link_to( "Enroll", he_enrolls_path,
+			:class => (controller_name == 'enrolls')?'current':nil))
+		l.push(link_to( "Interview", he_interviews_path,  
+			:class => (controller_name == 'interviews')?'current':nil))
+		l.push(link_to( "Samples", he_samples_path,
+			:class => (controller_name == 'samples')?'current':nil))
+		l.push(link_to( "Follow-Up", he_followups_path,
+			:class => (controller_name == 'followups')?'current':nil))
+		s << l.join("\n")
+		s << "\n</div><!-- mainmenu -->\n"
+	end
+
+	def application_root_menu
+		#<%# Page changes outside of ActionController go unnoticed %>
+		#<%# Due to the variability of the menu, we can't really cache it %>
+		#<%# cache 'page_menu' do %>
+		roots = Page.roots
+		count = roots.length
+		count = ( count > 0 ) ? count : 1 
+		width = ( 900 - count ) / count
+		s = "<div id='rootmenu' class='main_width'>\n"
+		roots.each do |page|
+			s << link_to( page.menu(session[:locale]), page.path, 
+				:style => "width: #{width}px",
+				:class => ((page == @page.try(:root))?'current':nil))
+			s << "\n"
+		end
+		s << "</div><!-- id='rootmenu' -->\n"
+	end
+
+	def application_sub_menu
+		if @page && !@page.root.children.empty?
+			s = "<div id='submenu' class='main_width'>\n"
+			s << "<div id='current_root'>"
+			s << @page.root.menu(session[:locale])
+			s << "</div>\n"
+			s << "<div id='children'>\n"
+			@page.root.children.each do |child|
+				s << "<span class='child#{(@page==child)?" current_child":""}'>"
+				s << link_to( child.menu(session[:locale]), child.path )
+				s << "</span>\n"
+			end
+			s << "</div><!-- id='children'  -->\n"
+			s << "</div><!-- id='submenu'  -->\n"
+		end
+	end
+
+	def application_home_page_pic
+		if @page && @page.is_home? && !@hpp.nil?
+			s = "<div id='home_page_pic' class='main_width'>\n"
+			s << image_tag( @hpp.image.url(:full) )
+			s << "\n</div><!-- id='home_page_pic'  -->\n"
+		end
+	end
+
 	#	This creates a button that looks like a submit button
 	#	but is just a javascript controlled link.
 	#	I don't like it.
---
+++ app/helpers/subject_helper.rb
@@ -0,0 +1,101 @@
+module SubjectHelper
+
+	def races_check_boxes
+		if Race.count > 0
+			s = "<fieldset id='races'>\n"
+			s << "<legend>Races</legend>\n"
+			s << "<ul>\n"
+			Race.all.each do |race|
+				s << "<li>"
+				s << check_box_tag( 'races[]', race.name, 
+						params['races'].try(:include?,race.name), 
+						:id => dom_id(race))
+				s << label_tag( dom_id(race), race.name )
+				s << "</li>\n"
+			end
+			s << "</ul>\n"
+			s << "</fieldset><!-- races -->\n"
+		end # if Race.count > 0
+	end
+
+	def subject_types_check_boxes
+		if SubjectType.count > 0
+			s = "<fieldset id='types'>\n"
+			s << "<legend>SubjectTypes</legend>\n"
+			s << "<ul>\n"
+			SubjectType.all.each do |type|
+				s << "<li>"
+				s << check_box_tag( 'types[]', type.name, 
+					params['types'].try(:include?,type.name), 
+					:id => dom_id(type))
+				s << label_tag( dom_id(type), type.name )
+				s << "</li>\n"
+			end
+			s << "</ul>\n"
+			s << "</fieldset><!-- types -->\n"
+		end # if SubjectType.count > 0
+	end
+
+	def dust_kit_radio_buttons
+		s = "<fieldset id='dust_kit'>\n"
+		s << "<legend>Dust Kit</legend>\n"
+		s << "<ul>\n"
+		s << "<li>"
+		s << radio_button_tag( 'dust_kit', 'ignore', 
+			params['dust_kit'].try(:==,'ignore') || params['dust_kit'].blank? )
+		s << label_tag( 'dust_kit_ignore', 'Ignore Dust Kit' )
+		s << "</li>\n"
+		s << "<li>"
+		s << radio_button_tag( 'dust_kit', 'none', 
+			params['dust_kit'].try(:==,'none') )
+		s << label_tag( 'dust_kit_none', 'No Dust Kit' )
+		s << "</li>\n"
+		s << "<li>"
+		s << radio_button_tag( 'dust_kit', 'shipped', 
+			params['dust_kit'].try(:==,'shipped') )
+		s << label_tag( 'dust_kit_shipped', 'Dust Kit Shipped' )
+		s << "</li>\n"
+		s << "<li>"
+		s << radio_button_tag( 'dust_kit', 'delivered', 
+			params['dust_kit'].try(:==,'delivered') )
+		s << label_tag( 'dust_kit_delivered', 'Dust Kit Delivered' )
+		s << "</li>\n"
+		s << "<li>"
+		s << radio_button_tag( 'dust_kit', 'returned', 
+			params['dust_kit'].try(:==,'returned') )
+		s << label_tag( 'dust_kit_returned', 'Dust Kit Returned' )
+		s << "</li>\n"
+		s << "<li>"
+		s << radio_button_tag( 'dust_kit', 'received', 
+			params['dust_kit'].try(:==,'received') )
+		s << label_tag( 'dust_kit_received', 'Dust Kit Received' )
+		s << "</li>\n"
+		s << "</ul>\n"
+		s << "</fieldset><!-- dust_kit -->\n"
+	end
+
+	def study_event_stuff
+		if StudyEvent.count > 0
+			s = ''
+			StudyEvent.all.each do |se|
+				s << "<fieldset class='study_event'>\n"
+				s << "<legend>#{se.description}</legend><ul>\n"
+				s << se_check_boxes(se,:eligible)
+				s << se_check_boxes(se,:chosen)
+				s << se_check_boxes(se,:consented)
+				s << se_check_boxes(se,:closed)
+				s << se_check_boxes(se,:terminated)
+				s << se_check_boxes(se,:completed)
+				s << "<li>Refused</li>\n"
+				s << "<li>Awaiting Letter</li>\n"
+				s << "<li>Sent Letter</li>\n"
+				s << "<li>Awaiting Interview</li>\n"
+				s << "<li>Interviewed</li>\n"
+				s << "</ul>\n"
+				s << "</fieldset><!-- study_event -->\n"
+			end
+			s
+		end	#	if StudyEvent.count > 0
+	end
+
+end
