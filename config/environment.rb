5890b9eff13ed0f0aee61e14de4543dda0a61b23
Jake <github@jake.otherinbox.com>
2010-07-13 15:32:02 -0700

    Commented out code that is now in the engine.

--- config/environment.rb
+++ config/environment.rb
@@ -113,9 +113,9 @@ end
 #       %y - Year without a century (00..99)
 #       %Y - Year with century
 #       %Z - Time zone name
-
-Time::DATE_FORMATS[:mdy] = "%b %d, %Y"   # Jan 01, 2009
-Date::DATE_FORMATS[:dob] = "%m/%d/%Y"   # 01/01/2009
-Time::DATE_FORMATS[:filename] = "%Y%m%d%H%M%S"   # 20091231235959
-
-
+#
+#Time::DATE_FORMATS[:mdy] = "%b %d, %Y"   # Jan 01, 2009
+#Date::DATE_FORMATS[:dob] = "%m/%d/%Y"   # 01/01/2009
+#Time::DATE_FORMATS[:filename] = "%Y%m%d%H%M%S"   # 20091231235959
+#
+#
