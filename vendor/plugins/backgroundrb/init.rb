e2691e72e295538d59f9fbb9da83ca531dc924a0
Jake <github@jake.otherinbox.com>
2010-07-28 17:06:58 -0700

    Added config.gem 'packet' and then wrapped require in config.after_initialize to allow rake gems:install to work.

--- vendor/plugins/backgroundrb/init.rb
+++ vendor/plugins/backgroundrb/init.rb
@@ -3,5 +3,9 @@
   $:.unshift File.join(directory,"app",code_dir)
 end
 
+config.gem 'packet'
+#	allow required gems to be loaded via rake gems:install
+config.after_initialize do
 require 'backgroundrb'
 #require "backgroundrb_status_controller"
+end
