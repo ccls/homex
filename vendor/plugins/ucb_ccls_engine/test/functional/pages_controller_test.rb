a851ddf9f3471a8e681aa8292d718d2d2a899589
Jake <github@jake.otherinbox.com>
2010-08-06 13:50:17 -0700

    Updated UCB CCLS Engine.

--- vendor/plugins/ucb_ccls_engine/test/functional/pages_controller_test.rb
+++ vendor/plugins/ucb_ccls_engine/test/functional/pages_controller_test.rb
@@ -215,16 +215,16 @@ end
 		assert_select 'title', page.title
 	end
 
-	test "should show HOME page with HPP" do
-		hpp = Factory(:home_page_pic,
-			:image_file_name => 'some_fake_file_name')
-		page = Page.by_path('/')
-		get :show, :id => page.id
-		assert_not_nil assigns(:hpp)
-		assert_template 'show'
-		assert_response :success
-		assert_select 'title', page.title
-	end
+#	test "should show HOME page with HPP" do
+#		hpp = Factory(:home_page_pic,
+#			:image_file_name => 'some_fake_file_name')
+#		page = Page.by_path('/')
+#		get :show, :id => page.id
+#		assert_not_nil assigns(:hpp)
+#		assert_template 'show'
+#		assert_response :success
+#		assert_select 'title', page.title
+#	end
 
 	test "should show HOME page without HPP" do
 		page = Page.by_path('/')
