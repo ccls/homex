b2d7e7830020d11a792439baae4c506dd13b5d6d
Jake <github@jake.otherinbox.com>
2010-08-16 12:21:58 -0700

    Using ruby_extension gem now.

--- vendor/plugins/ruby_extension/rails/init.rb
+++
@@ -1,8 +0,0 @@
-#	I need to be more explicit about which files to require
-#	because I have files of the same name in RAILS_ROOT/lib
-require "#{File.dirname(__FILE__)}/../lib/object"
-require "#{File.dirname(__FILE__)}/../lib/integer"
-require "#{File.dirname(__FILE__)}/../lib/array"
-require "#{File.dirname(__FILE__)}/../lib/string"
-require "#{File.dirname(__FILE__)}/../lib/hash"
-require "#{File.dirname(__FILE__)}/../lib/nil_class"
