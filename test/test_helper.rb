bd10c616fb175ba5f01b40d7e8ea87c809e6762f
Jake <github@jake.otherinbox.com>
2010-08-17 10:04:21 -0700

    turn_https_(on|off) is now in assert_this_and_that.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,15 +23,6 @@ class ActionController::TestCase
 
 	setup :turn_https_on
 
-	def turn_https_on
-		@request.env['HTTPS'] = 'on'
-#		@request.env['HTTP_X_FORWARDED_PROTO'] == 'https'
-	end
-
-	def turn_https_off
-		@request.env['HTTPS'] = nil
-	end
-
 	def assert_layout(layout)
 		layout = "layouts/#{layout}" unless layout.match(/^layouts/)
 		assert_equal layout, @response.layout
