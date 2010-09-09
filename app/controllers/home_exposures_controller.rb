ddc96cd0be3bc46a78721b8d71457b22742c7a15
Jake <github@jake.otherinbox.com>
2010-09-09 09:45:31 -0700

    Root path accessible to all.

--- app/controllers/home_exposures_controller.rb
+++ app/controllers/home_exposures_controller.rb
@@ -1,7 +1,11 @@
 class HomeExposuresController < ApplicationController
 
 #	before_filter :may_view_subjects_required
-	before_filter :may_view_required
+#	before_filter :may_view_required
+
+#	This is the root path.  There should be nothing
+#	here to block any user.  No filters!
+	skip_before_filter :login_required
 
 	def show
 		@page = Page.by_path("/")
--- test/functional/home_exposures_controller_test.rb
+++ test/functional/home_exposures_controller_test.rb
@@ -5,10 +5,13 @@ class HomeExposuresControllerTest < ActionController::TestCase
 	ASSERT_ACCESS_OPTIONS = { :actions => [:show] }
 
 	assert_access_with_login({
-		:logins => [:superuser,:admin,:editor,:interviewer,:reader] })
-	assert_no_access_with_login({
-		:logins => [:active_user] })
-	assert_no_access_without_login
+		:logins => [:superuser,:admin,:editor,
+			:interviewer,:reader,:active_user] })
+#		:logins => [:superuser,:admin,:editor,:interviewer,:reader] })
+#	assert_no_access_with_login({
+#		:logins => [:active_user] })
+#	assert_no_access_without_login
+	assert_access_without_login
 
 	assert_access_with_https
 	assert_no_access_with_http
