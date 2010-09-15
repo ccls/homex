9c4d9db01fae04b520866758101e0959ee5923a5
Jake <github@jake.otherinbox.com>
2010-09-15 15:34:00 -0700

    Working out specimen/sample screens.[#212]

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -9,6 +9,7 @@ class Sample < ActiveRecord::Base
 	has_many :aliquots
 	has_and_belongs_to_many :projects
 	has_one :sample_kit
+	accepts_nested_attributes_for :sample_kit
 
 #	how
 #	belongs_to :organization
@@ -18,14 +19,39 @@ class Sample < ActiveRecord::Base
 	validates_presence_of :sample_type_id, :sample_type
 	validates_presence_of :subject_id, :subject
 
-	stringify_date :sent_to_subject_on, :format => '%m/%d/%Y'
-	stringify_date :received_by_ccls_on, :format => '%m/%d/%Y'
-	stringify_date :sent_to_lab_on, :format => '%m/%d/%Y'
-	stringify_date :received_by_lab_on, :format => '%m/%d/%Y'
-	stringify_date :aliquotted_on, :format => '%m/%d/%Y'
+	stringify_date :sent_to_subject_on, 
+		:received_by_ccls_on, 
+		:sent_to_lab_on, 
+		:received_by_lab_on, 
+		:aliquotted_on, 
+		:format => '%m/%d/%Y'
 
 	def sample_type_parent
 		sample_type.parent
+	end
+
+	def kit_tracking_number
+		try(:sample_kit).try(:kit_package).try(:tracking_number)
+	end
+
+	def kit_sent_on
+		try(:sample_kit).try(:kit_package).try(:sent_on)
+	end
+
+	def kit_received_on
+		try(:sample_kit).try(:kit_package).try(:received_on)
+	end
+
+	def sample_tracking_number
+		try(:sample_kit).try(:sample_package).try(:tracking_number)
+	end
+
+	def sample_sent_on
+		try(:sample_kit).try(:sample_package).try(:sent_on)
+	end
+
+	def sample_received_on
+		try(:sample_kit).try(:sample_package).try(:received_on)
 	end
 
 end
