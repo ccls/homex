bc0270840d3b2452c8877313190a4e6b480070e9
Jake <github@jake.otherinbox.com>
2010-09-28 11:14:40 -0700

    Commented out unused method.

--- app/models/home_exposure_response.rb
+++ app/models/home_exposure_response.rb
@@ -5,11 +5,11 @@ class HomeExposureResponse < ActiveRecord::Base
 	validates_presence_of   :subject_id, :subject
 	validates_uniqueness_of :subject_id
 
-	def self.q_column_names
-		column_names - 
-			%w( id subject_id childid created_at updated_at ) -
-			%w( vacuum_bag_last_changed vacuum_used_outside_home )
-	end
+#	def self.q_column_names
+#		column_names - 
+#			%w( id subject_id childid created_at updated_at ) -
+#			%w( vacuum_bag_last_changed vacuum_used_outside_home )
+#	end
 
 	#	could have just used column_names, but these
 	#	are ordered to match the survey
