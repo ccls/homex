a260e55bbc6cfe27f113630f94c635ba5ae54f5a
Jake <github@jake.otherinbox.com>
2010-10-26 11:37:19 -0700

    Added simply_helpful.js

---
+++ public/javascripts/simply_helpful.js
@@ -0,0 +1,23 @@
+jQuery(function(){
+
+/*
+	This is no longer used.
+
+	jQuery('button.link').click(function(){
+		window.location.href = $(this).find('span.href').text();
+	});
+*/
+
+	jQuery('form.destroy_link_to').submit(function(){
+		var message = "Destroy?  Seriously?"
+		if( this.confirm && this.confirm.value ) {
+			message = this.confirm.value
+		}
+		if( !confirm(message) ){
+			return false;
+		}
+	});
+
+	jQuery('p.flash').click(function(){$(this).remove();});
+
+});
