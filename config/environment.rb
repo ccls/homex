462adbc4dc536848cfd1f8de416007189c1a0859
Jake <github@jake.otherinbox.com>
2010-11-09 11:43:46 -0800

    Reran ccls_engine generator.

--- config/environment.rb
+++ config/environment.rb
@@ -76,20 +76,3 @@ YNDK = HashWithIndifferentAccess.new({
 	:false => 2,
 	:dk    => 999
 }).freeze
-
-#	Due to some flexibility mods to CalnetAuthenticated
-#	User doesn't load until it is needed, 
-#	which means that calnet_authenticated isn't called,
-#	which means that current_user doesn't know what 
-#	a User is yet, which causes lots of ...
-#	NoMethodError (undefined method `find_create_and_update_by_uid' for nil:NilClass):
-#	so ...
-#	condition added to allow clean 'rake gems:install'
-if Gem.searcher.find('ccls_engine')
-require 'ccls_engine'	#	without this, rake has problems
-require 'user' unless defined?(User)
-#require 'role' unless defined?(Role)
-end
-#	Actually, this is probably only needed in development,
-#	but putting it in environments/development.rb doesn't
-#	work right, even in an after_initialize.
