1d116b1b974600888027e383b542036bfcdd3b02
Jake <github@jake.otherinbox.com>
2010-11-23 12:40:19 -0800

    Tests for gift card update.[#321]

--- test/functional/followup/subjects_controller_test.rb
+++ test/functional/followup/subjects_controller_test.rb
@@ -38,6 +38,41 @@ class Followup::SubjectsControllerTest < ActionController::TestCase
 		assert_not_nil @response.headers['Content-disposition'].match(/attachment;.*csv/)
 	end
 
+	test "should create subject's hx_gift_card if none " <<
+			"with #{u} login" do
+		login_as send(u)
+		subject = create_hx_subject
+		gift_card = create_gift_card(:project => Project['HomeExposures'])
+		assert_nil gift_card.subject
+		assert_nil subject.hx_gift_card
+		put :update, :id => subject.id, :gift_card => { :id => gift_card.id }
+		assert_equal subject.reload.hx_gift_card, gift_card.reload
+		assert_equal subject, assigns(:subject)
+		assert_redirected_to followup_subject_path(subject)
+	end
+
+	test "should change subject's hx_gift_card if exists " <<
+			"with #{u} login" do
+		login_as send(u)
+		subject = create_hx_subject
+		gift_card_1 = create_gift_card(:project => Project['HomeExposures'])
+		gift_card_2 = create_gift_card(:project => Project['HomeExposures'])
+		assert_nil gift_card_1.subject
+		assert_nil gift_card_2.subject
+		assert_nil subject.hx_gift_card
+		gift_card_1.update_attribute(:subject, subject)
+		assert_not_nil gift_card_1.subject
+		assert_nil     gift_card_2.subject
+		assert_not_nil subject.reload.hx_gift_card
+		put :update, :id => subject.id, :gift_card => { :id => gift_card_2.id }
+		assert_nil     gift_card_1.reload.subject
+		assert_not_nil gift_card_2.reload.subject
+		assert_not_nil subject.reload.hx_gift_card
+		assert_equal subject.reload.hx_gift_card, gift_card_2
+		assert_equal subject, assigns(:subject)
+		assert_redirected_to followup_subject_path(subject)
+	end
+
 end
 
 %w( active_user ).each do |u|
