e8899bbcc4530b4418afe711f25978d2eabb92c5
Jake <github@jake.otherinbox.com>
2010-12-07 17:09:56 -0800

    Reintegrated surveys.

--- config/initializers/surveyor.rb
+++ config/initializers/surveyor.rb
@@ -7,7 +7,7 @@ Surveyor::Config.run do |config|
 #	must use a symbol!
 #'/survey_finished' # "/surveys" # or :finish_path_method
   config['use_restful_authentication'] = false # set to true to use restful authentication
-  config['extend'] = %w(survey surveyor_controller response response_set)
+  config['extend'] = %w(surveyor_controller )
 end
 
 # require 'models/survey_extensions' # Extended the survey model
@@ -21,3 +21,17 @@ require 'survey'
 require 'surveyor_controller'
 require 'response'
 require 'response_set'
+
+ResponseSet.belongs_to :subject, :counter_cache => true, :foreign_key => 'study_subject_id'
+ResponseSet.has_one :survey_invitation
+ResponseSet.validates_presence_of :study_subject_id
+
+class ResponseSet
+# Convert response set to a home exposure questionnaire.
+def to_her
+HomeExposureResponse.create({
+:study_subject_id => self.study_subject_id
+}.merge(self.q_and_a_codes_as_attributes))
+end
+
+end
