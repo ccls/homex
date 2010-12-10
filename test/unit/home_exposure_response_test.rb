1e3dc8bdd7257f2217bdbfc6565f427649d687f2
Jake <github@jake.otherinbox.com>
2010-12-09 17:08:56 -0800

    Removed tests duplicated in ccls engine.

--- test/unit/home_exposure_response_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class HomeExposureResponseTest < ActiveSupport::TestCase
-
-	assert_should_create_default_object
-	assert_should_initially_belong_to(:subject)
-	assert_should_require_unique_attribute(:study_subject_id)
-
-end
