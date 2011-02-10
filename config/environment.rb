584f6f7763c8d58662c8280e00796d03bf2af560
Jake <github@jakewendt.com>
2011-02-09 16:04:40 -0800

    Upgraded to rails 2.3.11.  Cleaned up gems.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 #ENV['RAILS_ENV'] ||= 'production'
 
@@ -20,6 +20,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 RAILS_APP_NAME = 'homex'
 
 Rails::Initializer.run do |config|
+
+	config.gem 'RedCloth', :version => '!= 4.2.6'
 
 	if RUBY_PLATFORM =~ /java/
 		config.gem 'activerecord-jdbcsqlite3-adapter',
