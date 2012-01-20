6f05c78ac5138ca5aa2995ff6f695f66bd10b63e
Jake <github@jakewendt.com>
2012-01-20 12:37:04 -0800

    Removed unused tests.

--- test/functional/study_subjects_controller_test.rb
+++ test/functional/study_subjects_controller_test.rb
@@ -37,21 +37,6 @@ class StudySubjectsControllerTest < ActionController::TestCase
 	assert_access_with_https
 	assert_no_access_with_http
 
-#	TODO duplicate?
-	assert_no_access_with_login(
-		:attributes_for_create => nil,
-		:method_for_create => nil,
-		:actions => nil,
-		:suffix => " and invalid id DUPLICATE?",
-		:redirect => :study_subjects_path,
-		:login => :superuser,
-		:update => { :id => 0 },
-		:destroy => { :id => 0 },
-		:edit => { :id => 0 },
-		:show => { :id => 0 }
-	)
-
-#	%w( superuser admin editor interviewer reader ).each do |cu|
 	site_editors.each do |cu|
 
 		test "should update with #{cu} login" do
@@ -110,22 +95,6 @@ class StudySubjectsControllerTest < ActionController::TestCase
 			assert_template 'new'
 		end
 
-		test "should NOT create without race_id with #{cu} login" do
-			study_subject = create_study_subject
-			login_as send(cu)
-	pending
-	#		assert_difference('StudySubject.count',0){
-	#		assert_difference('SubjectType.count',0){
-	#		assert_difference('Race.count',0){
-	#			post :create, 
-	#				:study_subject => Factory.attributes_for(:study_subject,
-	#					:race_id => nil )
-	#		} } }
-	#		assert_not_nil flash[:error]
-	#		assert_response :success
-	#		assert_template 'new'
-		end
-
 		test "should NOT create without valid subject_type_id with #{cu} login" do
 			study_subject = create_study_subject
 			login_as send(cu)
@@ -141,59 +110,6 @@ class StudySubjectsControllerTest < ActionController::TestCase
 			assert_template 'new'
 		end
 
-		test "should NOT create without valid race_id with #{cu} login" do
-			study_subject = create_study_subject
-			login_as send(cu)
-	pending
-	#		assert_difference('StudySubject.count',0){
-	#		assert_difference('SubjectType.count',0){
-	#		assert_difference('Race.count',0){
-	#			post :create, 
-	#				:study_subject => Factory.attributes_for(:study_subject,
-	#					:race_id => 0 )
-	#		} } }
-	#		assert_not_nil flash[:error]
-	#		assert_response :success
-	#		assert_template 'new'
-		end
-
-
-##	TODO duplicate?
-#		test "should NOT update with #{cu} login" <<
-#			" and invalid" do
-#			study_subject = create_study_subject(:updated_at => ( Time.now - 1.day ) )
-#			StudySubject.any_instance.stubs(:valid?).returns(false)
-#			login_as send(cu)
-#			assert_difference('StudySubject.count',0){
-#			assert_difference('SubjectType.count',0){
-#			assert_difference('Race.count',0){
-#			deny_changes("StudySubject.find(#{study_subject.id}).updated_at") {
-#				put :update, :id => study_subject.id,
-#					:study_subject => {}
-#			} } } }
-#			assert_not_nil flash[:error]
-#			assert_response :success
-#			assert_template 'edit'
-#		end
-#
-##	TODO duplicate?
-#		test "should NOT update with #{cu} login" <<
-#			" and save fails" do
-#			study_subject = create_study_subject(:updated_at => ( Time.now - 1.day ) )
-#			StudySubject.any_instance.stubs(:create_or_update).returns(false)
-#			login_as send(cu)
-#			assert_difference('StudySubject.count',0){
-#			assert_difference('SubjectType.count',0){
-#			assert_difference('Race.count',0){
-#			deny_changes("StudySubject.find(#{study_subject.id}).updated_at") {
-#				put :update, :id => study_subject.id,
-#					:study_subject => {}
-#			} } } }
-#			assert_not_nil flash[:error]
-#			assert_response :success
-#			assert_template 'edit'
-#		end
-
 		test "should NOT update without subject_type_id with #{cu} login" do
 			study_subject = create_study_subject(:updated_at => ( Time.now - 1.day ) )
 			login_as send(cu)
@@ -207,22 +123,6 @@ class StudySubjectsControllerTest < ActionController::TestCase
 			assert_not_nil flash[:error]
 			assert_response :success
 			assert_template 'edit'
-		end
-
-		test "should NOT update without race_id with #{cu} login" do
-			study_subject = create_study_subject(:updated_at => ( Time.now - 1.day ) )
-			login_as send(cu)
-	pending
-	#		assert_difference('StudySubject.count',0){
-	#		assert_difference('SubjectType.count',0){
-	#		assert_difference('Race.count',0){
-	#		deny_changes("StudySubject.find(#{study_subject.id}).updated_at") {
-	#			put :update, :id => study_subject.id,
-	#				:study_subject => { :race_id => nil }
-	#		} } } }
-	#		assert_not_nil flash[:error]
-	#		assert_response :success
-	#		assert_template 'edit'
 		end
 
 		test "should NOT update without valid subject_type_id with #{cu} login" do
@@ -240,22 +140,6 @@ class StudySubjectsControllerTest < ActionController::TestCase
 			assert_template 'edit'
 		end
 
-		test "should NOT update without valid race_id with #{cu} login" do
-			study_subject = create_study_subject(:updated_at => ( Time.now - 1.day ) )
-			login_as send(cu)
-	pending
-	#		assert_difference('StudySubject.count',0){
-	#		assert_difference('SubjectType.count',0){
-	#		assert_difference('Race.count',0){
-	#		deny_changes("StudySubject.find(#{study_subject.id}).updated_at") {
-	#			put :update, :id => study_subject.id,
-	#				:study_subject => { :race_id => 0 }
-	#		} } } }
-	#		assert_not_nil flash[:error]
-	#		assert_response :success
-	#		assert_template 'edit'
-		end
-
 	end
 
 	non_site_editors.each do |cu|
@@ -266,54 +150,19 @@ class StudySubjectsControllerTest < ActionController::TestCase
 
 	site_readers.each do |cu|
 
-			test "study_subjects should include homex study subjects with #{cu} login" do
-				login_as send(cu)
-				study_subject = Factory(:study_subject)
-				Factory(:enrollment,
-					:project => Project['HomeExposures'],
-					:study_subject => study_subject)
-				get :index
-				#	'setup' creates a homex subject as well, so this won't work
-#				assert_equal [study_subject], assigns(:study_subjects)
-				#	but these should
-				assert assigns(:study_subjects).include?(study_subject)
-				assert_equal 2, assigns(:study_subjects).length
-			end
-
-
-
-
-#		test "should get index with study_subjects with #{cu} login" do
-#	#puts StudySubject.all.length
-#	#puts StudySubject.all.inspect
-#			assert_equal 1, StudySubject.for_hx.length		#	from :create_home_exposure_with_study_subject
-#			survey = Survey.find_by_access_code("home_exposure_survey")
-#			rs1 = rs2 = rs3 = rs4 = rs5 = nil
-#			assert_difference('StudySubject.count',3) {
-#			assert_difference('ResponseSet.count',5) {
-#				rs1 = fill_out_survey(:survey => survey)
-#				rs2 = fill_out_survey(:survey => survey, :study_subject => rs1.study_subject)
-#				rs2.to_her
-#				rs3 = fill_out_survey(:survey => survey)
-#				rs4 = fill_out_survey(:survey => survey, :study_subject => rs3.study_subject)
-#				rs5 = fill_out_survey(:survey => survey)
-#			} }
-#	#puts "About to fail"
-#	#puts StudySubject.for_hx.inspect
-#	#StudySubject.for_hx.each do |s|
-#	#puts s.enrollments.inspect
-#	#end
-#			assert_equal 1, StudySubject.for_hx.length	#	the survey doesn't make'em "for_hx"
-#			assert_difference('Project.count',1) {
-#				create_project	#Factory(:project)	#	test search code in view
-#			}
-#			#	There should now be 3 + 1 study_subjects in different states.
-#			login_as send(cu)
-#			get :index
-#			assert_equal 1, assigns(:study_subjects).length
-#			assert_response :success
-#			assert_template 'index'
-#		end
+		test "study_subjects should include homex study subjects with #{cu} login" do
+			login_as send(cu)
+			study_subject = Factory(:study_subject)
+			Factory(:enrollment,
+				:project => Project['HomeExposures'],
+				:study_subject => study_subject)
+			get :index
+			#	'setup' creates a homex subject as well, so this won't work
+#			assert_equal [study_subject], assigns(:study_subjects)
+			#	but these should
+			assert assigns(:study_subjects).include?(study_subject)
+			assert_equal 2, assigns(:study_subjects).length
+		end
 
 		test "should get index with order and dir desc with #{cu} login" do
 			login_as send(cu)
@@ -388,24 +237,6 @@ class StudySubjectsControllerTest < ActionController::TestCase
 			#	<a href="/study_subjects/2/events">events</a>
 			#	</div><!-- submenu -->
 		end
-
-	#		test "should NOT have hospital link if study_subject is not case "<<
-	#				"with #{cu} login" do
-	#			study_subject = create_study_subject
-	#			assert !study_subject.reload.is_case?
-	#			login_as send(cu)
-	#			get :show, :id => study_subject
-	#			assert_response :success
-	#			assert_template 'show'
-	#			assert_select "#submenu", :count => 1 do
-	#				assert_select "a", :count => 4
-	#				assert_select "a", 4
-	#				assert_select "a", :count => 0, :text => 'hospital'
-	#	#	apparently 0 and false don't work if :text exists
-	#	#			assert_select "a", 0, :text => 'hospital'
-	#	#			assert_select "a", false, :text => 'hospital'
-	#			end
-	#		end
 
 		test "should download csv with #{cu} login" do
 			login_as send(cu)
