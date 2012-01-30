73ea661308e7bc5248ef4eb06e25056ab84a4764
Jake <github@jakewendt.com>
2012-01-30 09:59:40 -0800

    Fixed now misnamed method.

--- test/unit/helpers/home_exposure_response_helper_test.rb
+++ test/unit/helpers/home_exposure_response_helper_test.rb
@@ -4,7 +4,7 @@ class HomeExposureResponseHelperTest < ActionView::TestCase
 
 	test "should show row without value" do
 		@he = Factory(:home_exposure_response)
-		HomeExposureResponse.field_names.each do |field_name|
+		HomeExposureResponse.db_field_names.each do |field_name|
 			response = HTML::Document.new(show_row(:he,field_name)).root
 			assert_select response, 'tr', 1 do
 				assert_select 'td', 3
@@ -14,7 +14,7 @@ class HomeExposureResponseHelperTest < ActionView::TestCase
 
 	test "should show row with value" do
 		@he = Factory(:home_exposure_response)
-		HomeExposureResponse.field_names.each do |field_name|
+		HomeExposureResponse.db_field_names.each do |field_name|
 			response = HTML::Document.new(
 				show_row(:he,field_name,:value => 'fake')).root
 			assert_select response, 'tr', 1 do
